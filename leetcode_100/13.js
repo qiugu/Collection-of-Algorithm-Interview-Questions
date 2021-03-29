52b1ab85f300dcb2b3d9b4906214bf3a470f0dd0
qiugu <476710564@qq.com>
2021-03-29 22:15:51 +0800

    feat: add three problems

---
+++ leetcode_100/13.js
@@ -0,0 +1,27 @@
+/**
+ * 罗马数字转整数
+ * @param {*} s 
+ * @returns 
+ */
+var romanToInt = function(s) {
+  const symbols = {
+      "M": 1000,
+      "D": 500, 
+      "C": 100,
+      "L": 50, 
+      "X": 10, 
+      "V": 5,
+      "I": 1
+  };
+  let ans = 0;
+  for(let i = 0; i < s.length; i++) {
+      if (symbols[s[i]] < symbols[s[i+1]]) { 
+          ans -= symbols[s[i]]; 
+      } else {
+          ans += symbols[s[i]];
+      }
+  }
+  return ans;
+};
+
+console.log(romanToInt('IV'));
