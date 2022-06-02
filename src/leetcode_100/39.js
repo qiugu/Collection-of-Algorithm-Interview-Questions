691fc12ec0fe3fa213cea3ada18fd09b5b82f877
qiugu <476710564@qq.com>
2022-06-02 20:40:15 +0800

    refactor: add detail notes on No.39

--- src/leetcode_100/39.js
+++ src/leetcode_100/39.js
@@ -6,27 +6,36 @@
  * @returns 
  */
 var combinationSum = function(candidates, target) {
-  /**
-      回溯?
-   */
-  console.time();
   const ans = [];
-  const backtrace = (target, combine, idx) => {
-      if (idx === candidates.length) return;
-      if (target === 0) {
-          ans.push(combine);
-          return;
-      }
-      // 不选当前值，直接跳过
-      backtrace(target, combine, idx+1);
-      // 选择当前值，剩余target需要大于0
-      if (target - candidates[idx] >= 0) {
-          // 因为可以重复选择，所以idx不需要递增
-          backtrace(target - candidates[idx], [...combine, candidates[idx]], idx);
-      }
+  /**
+   * 回溯
+   * 1. 如何判断每次回溯的结束条件
+   * 2. 注意索引超出数组长度时要退出回溯
+   * 3. 做出选择，如果选择当前数为构成组合，不选择当前数
+   * 4. 选择当前数组合的时候，索引不变，因为每个数都可以重复使用
+   * @param {*} target 目标和
+   * @param {*} nums 当前得到的组合
+   * @param {*} index candidate中的索引
+   */
+  const backtrace = (target, nums, index) => {
+    // 如果索引已经超出给定数组的长度，说明已经遍历结束了
+    // if (index === candidates.length) return;
+    // 如果目标和为0，说明当前的组合满足条件，则加入结果数组中
+    if (target === 0) {
+        ans.push(nums);
+        return;
+    }
+    // 进行选择，如果不选当前索引的值
+    backtrace(target, nums, index+1);
+    // 如果选择当前值，则目标值需减去当前值会得到一个新的目标值，并把该值加入组合中
+    const remaining = target - candidates[index];
+    // 判断剩余目标值是否大于等于0，为什么要加上等于？没有等于的话，不会进入上面的步骤
+    if (remaining >= 0) {
+        // 注意这里idx不变，因为每个数可以使用多次
+        backtrace(remaining, [...nums, candidates[index]], index);
+    }
   };
   backtrace(target, [], 0);
-  console.timeEnd();
   return ans;
 };
 
